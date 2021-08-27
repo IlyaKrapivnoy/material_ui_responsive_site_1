701bccea17b828c70cabb6efddd8fbae7e071383
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-27 21:35:37 +0300

    add modal on llogin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import {
     AppBar,
     Button,
+    Container,
     makeStyles,
     Modal,
     Toolbar,
     Typography,
 } from '@material-ui/core';
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
     nav: {
@@ -29,10 +31,28 @@ const useStyles = makeStyles((theme) => ({
             display: 'block',
         },
     },
+    modal: {
+        backgroundColor: '#eee',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 400,
+        height: 500,
+        margin: '0 auto',
+    },
 }));
 
 const Header = () => {
     const classes = useStyles();
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleOpen = () => {
+        setOpenModal(true);
+    };
+
+    const handleClose = () => {
+        setOpenModal(false);
+    };
 
     return (
         <div>
@@ -44,7 +64,37 @@ const Header = () => {
                     <Typography variant='h6' className={classes.logoSm}>
                         BlackWood
                     </Typography>
-                    <Button color='inherit'>Login</Button>
+                    <Button color='inherit' onClick={handleOpen}>
+                        Login
+                    </Button>
+                    {/* <Modal className={classes.modal}></Modal> */}
+                    <Modal
+                        style={{
+                            top: '20%',
+                        }}
+                        aria-labelledby='spring-modal-title'
+                        aria-describedby='spring-modal-description'
+                        className={classes.modal}
+                        open={openModal}
+                        // onClose={handleClose}
+                        closeAfterTransition
+                        // BackdropComponent={Backdrop}
+                        BackdropProps={{
+                            timeout: 500,
+                        }}
+                    >
+                        <Container>
+                            <div className={classes.paper}>
+                                <h2 id='spring-modal-title'>Spring modal</h2>
+                                <p id='spring-modal-description'>
+                                    react-spring animates me.
+                                </p>
+                                <Button color='inherit' onClick={handleClose}>
+                                    Close
+                                </Button>
+                            </div>
+                        </Container>
+                    </Modal>
                 </Toolbar>
             </AppBar>
         </div>
