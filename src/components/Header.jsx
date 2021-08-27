9183bb73bc9ac43276a6fab02e18d7818e6db7c2
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-27 21:17:44 +0300

    add appbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,52 @@
-import { Button, makeStyles } from '@material-ui/core';
+import {
+    AppBar,
+    Button,
+    makeStyles,
+    Modal,
+    Toolbar,
+    Typography,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    button: {
-        color: 'red',
+    nav: {
+        backgroundColor: '#313131',
+    },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    logoLg: {
+        letterSpacing: 3,
+    },
+    logoSm: {
+        display: 'none',
+    },
+    [theme.breakpoints.down('xs')]: {
+        logoLg: {
+            display: 'none',
+        },
+        logoSm: {
+            display: 'block',
+        },
     },
 }));
 
 const Header = () => {
     const classes = useStyles();
+
     return (
         <div>
-            <Button variant='contained' size='small' className={classes.button}>
-                test
-            </Button>
+            <AppBar position='static' className={classes.nav}>
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant='h6' className={classes.logoLg}>
+                        &#10065; BlackWood
+                    </Typography>
+                    <Typography variant='h6' className={classes.logoSm}>
+                        BlackWood
+                    </Typography>
+                    <Button color='inherit'>Login</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 };
