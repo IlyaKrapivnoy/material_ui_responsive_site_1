004d7c610d60c36c12384a00fd17f3717e6417f7
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-29 02:30:43 +0300

    add cards field

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,10 @@
 import {
+    Box,
     Breadcrumbs,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
     Container,
     Link,
     makeStyles,
@@ -11,6 +16,34 @@ const useStyles = makeStyles((theme) => ({
     content: { backgroundColor: '#f5f5f5', height: '100vh' },
     container: {
         paddingTop: theme.spacing(10),
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    cover: {
+        width: 151,
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+    },
+    playIcon: {
+        height: 38,
+        width: 38,
+    },
+    cardField: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '40px 0',
+    },
+    card: {
+        width: '30%',
     },
 }));
 
@@ -34,6 +67,98 @@ const Content = () => {
                     </Link>
                     <Typography color='textPrimary'>Partnership</Typography>
                 </Breadcrumbs>
+                <Box className={classes.cardField}>
+                    <Card className={classes.card}>
+                        <CardActionArea>
+                            <CardMedia
+                                component='img'
+                                alt='Contemplative Reptile'
+                                height='140'
+                                image='https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg'
+                                square='true'
+                                title='Contemplative Reptile'
+                            />
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant='h5'
+                                    component='h2'
+                                >
+                                    Card 1
+                                </Typography>
+                                <Typography
+                                    variant='body2'
+                                    color='textSecondary'
+                                    component='p'
+                                >
+                                    Lizards are a widespread group of squamate
+                                    reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                    <Card className={classes.card}>
+                        <CardActionArea>
+                            <CardMedia
+                                component='img'
+                                alt='Contemplative Reptile'
+                                height='140'
+                                image='https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg'
+                                square='true'
+                                title='Contemplative Reptile'
+                            />
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant='h5'
+                                    component='h2'
+                                >
+                                    Card 2
+                                </Typography>
+                                <Typography
+                                    variant='body2'
+                                    color='textSecondary'
+                                    component='p'
+                                >
+                                    Lizards are a widespread group of squamate
+                                    reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                    <Card className={classes.card}>
+                        <CardActionArea>
+                            <CardMedia
+                                component='img'
+                                alt='Contemplative Reptile'
+                                height='140'
+                                image='https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg'
+                                square='true'
+                                title='Contemplative Reptile'
+                            />
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant='h5'
+                                    component='h2'
+                                >
+                                    Card 3
+                                </Typography>
+                                <Typography
+                                    variant='body2'
+                                    color='textSecondary'
+                                    component='p'
+                                >
+                                    Lizards are a widespread group of squamate
+                                    reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Box>
             </Container>
         </div>
     );
